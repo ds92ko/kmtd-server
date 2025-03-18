433c74b8bd002d8b41d53cfdbde31686ca173b6d
dasomko <ds92ko@gmail.com>
2025-03-18 19:09:36 +0900

    chore: port 변경

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(
 );
 app.use('/api/todos', todoRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
